e29d844e6fd0c9ee05248c528f4db018b6e39412
KarelSlgd <20213tn076@utez.edu.mx>
2024-10-03 15:42:43 -0600

    chanenames

--- lib/navigation/profile.dart
+++ lib/navigation/profile.dart
@@ -11,8 +11,78 @@ class Profile extends StatelessWidget {
       appBar: AppBar(
         title: const Text('profile'),
       ),
-      body: const Center(
-        child: Text('profile'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,
+                    child: Text("KS"),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "karelslgd",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Galantis - In My Head",
+                        style: TextStyle(color: Colors.black54),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Icon(Icons.more_vert)
+                ],
+              ),
+            ),
+            Image.asset('assets/cat.jpg', width: double.infinity, height: 400),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Icon(Icons.favorite_outline),
+                  SizedBox(
+                    width: 16.0,
+                  ),
+                  Icon(Icons.message_outlined),
+                  SizedBox(
+                    width: 16.0,
+                  ),
+                  Icon(Icons.ios_share_outlined),
+                  Spacer(),
+                  Icon(Icons.bookmark_outline)
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Karel Mendoza",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("Ver más comentarios"),
+                  Text(
+                    "Hace 3 horas",
+                    style: TextStyle(color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
